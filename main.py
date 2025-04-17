fix exploding gradient, add model checkpoint save and loading model weights from file

main.py
@@ -1,18 +1,40 @@
 import numpy as np
 import argparse
 
-from src.models import MultiLayerPerceptron
-from src.utils import CategoricalCrossEntropy
+from src.models import MultiLayerPerceptron, ModelCheckpoint
+from src.utils import CategoricalCrossEntropy, Accuracy
 from src.dataset import Dataset
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--early_stopping",
+    type=int,
+    default=None,
+    help="Number of epochs to wait until stop training for no progress"
+)
+parser.add_argument(
+    "--name",
+    type=str,
+    default="model",
+    help="Model name"
+)
+parser.add_argument(
+    "--model_weights",
+    type=str,
+    default=None,
+    help="Path to model weights npz file"
+)
+args = parser.parse_args()
 
 
 def generate_data(
     n_samples: int,
     n_features: int,
     classes_nb: int,
-    seed: int | None = None
+    seed: int = 0
 ):
-    if seed is not None:
+    if seed:
         np.random.seed(seed)
     
     samples_per_class = n_samples // classes_nb
@@ -28,46 +50,53 @@ def generate_data(
         data.append(class_samples)
         labels.append(class_labels)
 
-    print(data[0].shape, labels[0].shape)
-
     data = np.vstack(data)
     labels = np.concatenate(labels)
 
     onehot_labels = np.zeros((labels.size, classes_nb))
     onehot_labels[np.arange(labels.size), labels] = 1
 
-    return data, labels
+    return data, onehot_labels
 
 
 def train(model: MultiLayerPerceptron, dataset: Dataset):
     learning_rate = 0.001
     cat_crossentropy = CategoricalCrossEntropy()
-    epochs = 100
+    epochs = 20
+    metric = Accuracy()
     batch_size = {"val": 64, "train": 32}
+    checkpoint = ModelCheckpoint(
+        model, "accuracy", "max", args.early_stopping
+    )
 
     dataset.split(val_factor=0.15, shuffle=True)
+    dataset.normalize()
 
-    best_loss = float("inf")
     for epoch in range(epochs):
         print(20 * "-" + f"Epoch: {epoch}/{epochs}" + 20 * "-")
         for learning_step in ["train", "val"]:
-            loss = 0.0
+            loss, acc = 0.0, 0.0
             batch_data = dataset.get_batches(
                 learning_step, batch_size[learning_step]
             )
             for data, labels in batch_data:
                 predictions = model.predict(data)
-                loss += cat_crossentropy(labels, predictions)
+                loss += cat_crossentropy(predictions, labels)
+                acc += metric(predictions, labels)
                 if learning_step == "train":
-                    model.backprogation(labels, cat_crossentropy, learning_rate)
-            
-            print(f"{learning_step.upper()}_loss: {loss / len(batch_data)}")
+                    model.backprogation(labels, learning_rate)
 
-        # model checkpoint based on validation loss
+            loss /= len(batch_data)
+            acc /= len(batch_data)
+            print(f"{learning_step.upper()} - loss: {loss} acc: {acc}")
 
-
-def eval():
-    ...
+            if learning_step == "val":
+                should_stop = checkpoint.check_improvement(acc)
+                break
+        if should_stop:
+            print(f"Finished training on {epoch} epoch")
+            break
+    checkpoint.save_model(f"./models/{args.name}.npz")
 
 
 if __name__ == "__main__":
@@ -76,10 +105,12 @@ if __name__ == "__main__":
     data, labels = generate_data(
         n_samples=500,
         n_features=n_features,
-        classes_nb=classes_nb
+        classes_nb=classes_nb,
+        seed=42
     )
     dataset = Dataset(data, labels)
     model = MultiLayerPerceptron(input_size=n_features, classes_nb=classes_nb)
     print("Model architecture: ", model)
-
+    if args.model_weights:
+        model.load_weights(args.model_weights)
     train(model, dataset)

src/dataset/dataset.py
@@ -10,23 +10,26 @@ class Dataset:
         self.data = data
         self.labels = labels
     
-    def split(self, val_factor: float = 0.2, shuffle: bool = True):
+    def split(self, val_factor: float = 0.2, shuffle: bool = True, seed: int = 0):
+        if seed:
+            np.random.seed(seed)
+
         samples_nb = self.data.shape[0]
-        val_size = int(samples_nb) * val_factor
+        val_size = int(samples_nb * val_factor)
         indices = np.arange(samples_nb)
 
         if shuffle:
             np.random.shuffle(indices)
-        
+
         val_indices = indices[:val_size]
         train_indices = indices[val_size:]
 
-        self.train_subset = (
+        self.train_subset = [
             self.data[train_indices], self.labels[train_indices]
-        )
-        self.val_subset = (self.data[val_indices], self.labels[val_indices])
+        ]
+        self.val_subset = [self.data[val_indices], self.labels[val_indices]]
 
-    def get_batches(self, subset_name: str | None, batch_size: int):
+    def get_batches(self, subset_name: str, batch_size: int):
         if subset_name == "train":
             data, labels = self.train_subset
         elif subset_name == "val":
@@ -43,3 +46,10 @@ class Dataset:
             batches.append((batch_data, batch_labels))
 
         return batches
+    
+    def normalize(self):
+        mean = self.train_subset[0].mean(axis=0)
+        std = self.train_subset[0].std(axis=0)
+
+        self.train_subset[0] = (self.train_subset[0] - mean) / (std + 1e-8)
+        self.val_subset[0] = (self.val_subset[0] - mean) / (std + 1e-8)

src/models/layers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections.abc import Callable
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 
@@ -16,7 +17,7 @@ class InputLayer:
         )
 
 
-class BaseLayer:
+class BaseLayer(ABC):
     def __init__(
         self,
         features_in_size: int,
@@ -25,7 +26,7 @@ class BaseLayer:
     ):
         self.features_in_size = features_in_size
         self.features_out_size = features_out_size
-        self.weights = np.random.randn(self.features_in_size, self.features_out_size) * 0.01
+        self.weights = None
         self.biases = np.zeros((self.features_out_size,))
         self.activation = activation
         self.input = None
@@ -38,30 +39,72 @@ class BaseLayer:
 
         return self.output
 
-    def update_params(self, learning_rate: float):
+    def update_params(self, learning_rate: float, momentum: float = 0.9):
         if self.input is not None and self.delta is not None:
             self.weights -= learning_rate * self.input.T @ self.delta
-            self.biases -= learning_rate * np.sum(self.delta, axis=0, keepdims=True)
+            self.biases -= learning_rate * np.sum(self.delta, axis=0)
     
+    def clip_gradient(self, clip_value: float = 5.0):
+        self.delta = np.clip(self.delta, -clip_value, clip_value)
+
     def __repr__(self):
         return (
             "{}"
-            f"({self.features_in_size} -> {self.features_out_size})"
+            f"({self.features_in_size} -> {self.features_out_size}) "
             f"activation={self.activation.__class__.__name__}"
         )
+    
+    @abstractmethod
+    def backward(*args):
+        ...
 
 
 class HiddenLayer(BaseLayer):
-    def backward(self, next_delta: np.ndarray) -> np.ndarray:
-        error = next_delta @ self.weights.T
+    instances_nb = 0 
+    def __init__(
+        self,
+        features_in_size: int,
+        features_out_size: int,
+        activation: Callable
+    ):
+        super().__init__(features_in_size, features_out_size, activation)
+        # He initialization
+        self.weights = (
+            np.random.randn(self.features_in_size, self.features_out_size)
+            * np.sqrt(2 / self.features_in_size)
+        )
+        self.name = f"{self.__class__.__name__}_{HiddenLayer.instances_nb}"
+        HiddenLayer.instances_nb += 1
+
+    def backward(self, next_delta: np.ndarray, next_weights: np.ndarray) -> np.ndarray:
+        error = next_delta @ next_weights.T
         self.delta = error * self.activation.derivative(self.output)
+        self.clip_gradient(clip_value=10.0)
 
         return self.delta
-    
+
     def __repr__(self):
-        return super().__repr__().format("HiddenLayer")
+        return super().__repr__().format(self.__class__.__name__)
 
 
 class OutputLayer(BaseLayer):
+    def __init__(
+        self,
+        features_in_size: int,
+        features_out_size: int,
+        activation: Callable
+    ):
+        super().__init__(features_in_size, features_out_size, activation)
+        # Xavier/Glorot initialization
+        self.weights = (
+            np.random.randn(self.features_in_size, self.features_out_size)
+            * np.sqrt(1 / self.features_in_size)
+        )
+
+    def backward(self, y: np.ndarray) -> np.ndarray:
+        self.delta = self.output - y
+        self.clip_gradient(clip_value=1.0)
+        return self.delta
+
     def __repr__(self):
         return super().__repr__().format("OutputLayer")

src/models/mlp.py
@@ -6,43 +6,66 @@ from .layers import HiddenLayer, OutputLayer, InputLayer
 
 class MultiLayerPerceptron:
     def __init__(self, input_size: int, classes_nb: int):
-        self.input_layer = InputLayer(input_size)
-        self.layer1 = HiddenLayer(self.input_layer.input_size, 16, ReLU())
-        self.layer2 = HiddenLayer(16, 8, ReLU())
-        self.out_layer = OutputLayer(8, classes_nb, Softmax())
+        self.layers = [
+            InputLayer(input_size),
+            HiddenLayer(input_size, 16, ReLU()),
+            HiddenLayer(16, 8, ReLU()),
+            OutputLayer(8, classes_nb, Softmax())
+        ]
     
     def predict(self, input: np.ndarray) -> np.ndarray:
-        self.input = input
-        output = self.layer1.forward(self.input)
-        output = self.layer2.forward(output)
-        output = self.out_layer(output)
+        output = input
+        for layer in self.layers:
+            output = layer.forward(output) 
 
         return output
 
-    def backprogation(self, y: np.ndarray, loss: object, learning_rate: float):
+    def backprogation(self, y: np.ndarray, learning_rate: float):
         # layers backpropagation
-        self.out_layer.delta = loss.derivative(y, self.out_layer.output)
-        h2_delta = self.layer2.backward(self.out_layer.delta)
-        h1_delta = self.layer1.backward(h2_delta)
+        next_delta = self.layers[-1].backward(y)
+        next_weights = self.layers[-1].weights
+        for layer in reversed(self.layers[1:-1]):
+            next_delta = layer.backward(next_delta, next_weights)
+            next_weights = layer.weights
 
         # layers parameters update
-        self.layer1.update_params(learning_rate)
-        self.layer2.update_params(learning_rate)
-        self.out_layer.update_params(learning_rate)
+        for layer in self.layers[1:]:
+            layer.update_params(learning_rate)
     
     def __repr__(self):
         arch = f"{self.__class__.__name__}(\n"
-        layers = [
-            self.input_layer, self.layer1, self.layer2, self.output_layer
-        ]
-        for i, layer in layers:
-            arch += f"\t[{i}] {repr(layer)}"
+        for i, layer in enumerate(self.layers):
+            arch += f"\t[{i}] {repr(layer)}\n"
         arch += ")"
 
         return arch
 
-    def save_model(self):
-        ...
-    
-    def load_model(self):
-        ...
+    def get_weights(self):
+        weights = {}
+        for i, layer in enumerate(self.layers):
+            if hasattr(layer, "name"):
+                key = layer.name
+            else:
+                key = layer.__class__.__name__
+            if hasattr(layer, "weights") and hasattr(layer, "biases"):
+                weights[key] = {
+                    "weights": layer.weights,
+                    "biases": layer.biases
+                }
+
+        return weights
+
+    def load_weights(self, file_path: str):
+        weights = np.load(file_path, allow_pickle=True)
+        self.layer_names = [
+            layer.name if hasattr(layer, "name") else None
+            for layer in self.layers
+        ]
+        for layer_name, attrs in weights.items():
+            try:
+                layer_idx = self.layer_names.index(layer_name)
+                attrs = attrs.item()
+            except ValueError:
+                continue
+            self.layers[layer_idx].weights = attrs["weights"].copy()
+            self.layers[layer_idx].biases = attrs["biases"].copy()

src/utils/activations.py
@@ -1,29 +1,18 @@
 import numpy as np
 
 
-class Sigmoid:
-    def __call__(self, input: np.ndarray) -> np.ndarray:
-        return 1 / (1 + np.exp(-input))
-    
-    def derivative(self, input: np.ndarray) -> np.ndarray:
-        s = self(input)
-        df = s * (1 - s)
-
-        return df
-
-
 class ReLU:
     def __call__(self, input: np.ndarray) -> np.ndarray:
         return np.maximum(0, input)
     
     def derivative(self, input: np.ndarray) -> np.ndarray:
-        return (input > 0).astype(float)
+        return np.where(input > 0, 1.0, 0.0)
 
 
 class Softmax:
     def __call__(self, input: np.ndarray) -> np.ndarray:
         input -= np.max(input, axis=1, keepdims=True)
         exponential = np.exp(input)
-        probs = exponential / np.sum(exponential, axis=1, keepdims=True)
+        probs = exponential / (np.sum(exponential, axis=1, keepdims=True) + 1e-10)
 
         return probs

src/utils/losses.py
@@ -1,38 +1,14 @@
 import numpy as np
 
 
-class BinaryCrossEntropy:
-    def __init__(self, epsilon: float = 1e-15):
-        self.min = epsilon
-        self.max = 1 - epsilon
-
-    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
-        y_pred = np.clip(y_pred, self.min, self.max)
-        loss = y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)
-        loss = -np.mean(loss)
-
-        return loss
-
-    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-        y_pred = np.clip(y_pred, self.min, self.max)
-        df = (y_pred - y_true) / (y_pred * (1 - y_pred)) / y_true.shape[0]
-
-        return df
-
-
 class CategoricalCrossEntropy:
     def __init__(self, epsilon: float = 1e-15):
         self.min = epsilon
         self.max = 1 - epsilon
-    
+
     def __call__(self, y_pred: np.ndarray, y_true: np.ndarray) -> float:
         y_pred = np.clip(y_pred, self.min, self.max)
         loss = -np.sum(y_true * np.log(y_pred), axis=-1)
+        loss = np.mean(loss)
 
         return loss
-
-    def derivative(self, y_pred: np.ndarray, y_true: np.ndarray):
-        y_pred = np.clip(y_pred, self.min, self.max)
-        df = -y_true / y_pred
-
-        return df